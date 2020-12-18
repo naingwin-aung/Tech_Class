b6f79d720549d258b94dce6f12cb3451b1604d92
NaingWinAung <naingwinaung1710@gmail.com>
2020-12-18 19:23:53 +0630

    checkout modify

--- dist/main.48b3db1d.js
+++ dist/main.48b3db1d.js
@@ -150,7 +150,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55765" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54222" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
